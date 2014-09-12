d4f310d0b525e3b1ed0e69ddad5ad4cd459c029c
James Benton <james.benton2@gmail.com>
2014-09-12 17:02:53 +0100

    Fix ROSE_DATA_PATH

--- client/js/loaders/rose.js
+++ client/js/loaders/rose.js
@@ -5,7 +5,7 @@ var ROSELoader = {};
 ROSELoader.load = function(path, callback) {
   var loader = new THREE.XHRLoader();
   loader.setResponseType('arraybuffer');
-  loader.load(ROSE_DATA_BASE + path, function (buffer) {
+  loader.load(ROSE_DATA_PATH + path, function (buffer) {
     callback(new BinaryReader(buffer));
   });
 };
