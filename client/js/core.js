48a0c87f69c195f58571ed814780efd2f9a9222b
James Benton <james.benton2@gmail.com>
2014-09-12 17:04:37 +0100

    Refactor to loaders/him.js

--- client/js/core.js
+++ client/js/core.js
@@ -56,29 +56,6 @@ CameraAnimationHandler.prototype.update = function(delta) {
   this.camera.lookAt(new THREE.Vector3(targetPos.x, targetPos.y, targetPos.z));
 };
 
-
-
-function HIMData() {
-  this.width = 0;
-  this.height = 0;
-  this.map = [];
-}
-var HIMLoader = {};
-HIMLoader.load = function(path, callback) {
-  ROSELoader.load(path, function(b) {
-    var data = new HIMData();
-    data.width = b.readUint32();
-    data.height = b.readUint32();
-    /*patchGridCount*/ b.readUint32();
-    /*patchSize*/ b.readFloat();
-    for (var i = 0; i < data.width*data.height; ++i) {
-      data.map.push(b.readFloat());
-    }
-    callback(data);
-  });
-};
-
-
 var IFOBLOCKTYPE = {
   MapInformation: 0,
   Object: 1,
