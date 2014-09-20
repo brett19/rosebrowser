73c2ffe787d4ea568d1ac1b293e866e3900406a6
James Benton <james.benton2@gmail.com>
2014-09-20 04:20:09 +0100

    Add functions to convert from znzin blend type to three.js

--- client/js/loaders/rose.js
+++ client/js/loaders/rose.js
@@ -36,3 +36,76 @@ if (EXPERIMENTAL_WS_LOAD) {
   };
 
 }
+
+var ZZ_BLEND = {
+  ZERO: 1,
+  ONE: 2,
+  SRC_COLOR: 3,
+  INV_SRC_COLOR: 4,
+  SRC_ALPHA: 5,
+  INV_SRC_ALPHA: 6,
+  DEST_ALPHA: 7,
+  INV_DEST_ALPHA: 8,
+  DEST_COLOR: 9,
+  INV_DEST_COLOR: 10,
+  SRC_ALPHA_SAT: 11,
+  BOTH_SRC_ALPHA: 12,
+  BOTH_INV_SRC_ALPHA: 13,
+  BLEND_FACTOR: 14,
+  INV_BLEND_FACTOR: 15
+};
+
+function convertZnzinBlendType(znzin) {
+  switch(znzin) {
+    case ZZ_BLEND.ZERO:
+      return THREE.ZeroFactor;
+    case ZZ_BLEND.ONE:
+      return THREE.OneFactor;
+    case ZZ_BLEND.SRC_COLOR:
+      return THREE.SrcColorFactor;
+    case ZZ_BLEND.INV_SRC_COLOR:
+      return THREE.OneMinusSrcColorFactor;
+    case ZZ_BLEND.SRC_ALPHA:
+      return THREE.SrcAlphaFactor;
+    case ZZ_BLEND.INV_SRC_ALPHA:
+      return THREE.OneMinusSrcAlphaFactor;
+    case ZZ_BLEND.DEST_ALPHA:
+      return THREE.DstAlphaFactor;
+    case ZZ_BLEND.INV_DEST_ALPHA:
+      return THREE.OneMinusDstAlphaFactor;
+    case ZZ_BLEND.DEST_COLOR:
+      return THREE.DstColorFactor;
+    case ZZ_BLEND.INV_DEST_COLOR:
+      return THREE.OneMinusDstColorFactor;
+    case ZZ_BLEND.SRC_ALPHA_SAT:
+      return THREE.SrcAlphaSaturateFactor;
+    case ZZ_BLEND.BOTH_SRC_ALPHA:
+    case ZZ_BLEND.BOTH_INV_SRC_ALPHA:
+    case ZZ_BLEND.BLEND_FACTOR:
+    case ZZ_BLEND.INV_BLEND_FACTOR:
+      throw 'Unsupported znzin blend type ' + znzin;
+  }
+}
+
+var ZZ_BLEND_OP = {
+  ADD: 1,
+  SUBTRACT: 2,
+  REV_SUBTRACT: 3,
+  MIN: 4,
+  MAX: 5
+};
+
+function convertZnzinBlendOp(znzin) {
+  switch(znzin) {
+    case ZZ_BLEND_OP.ADD:
+      return THREE.AddEquation;
+    case ZZ_BLEND_OP.SUBTRACT:
+      return THREE.SubtractEquation;
+    case ZZ_BLEND_OP.REV_SUBTRACT:
+      return THREE.ReverseSubtractEquation;
+    case ZZ_BLEND_OP.MIN:
+      return THREE.MinEquation;
+    case ZZ_BLEND_OP.MAX:
+      return THREE.MaxEquation;
+  }
+}
