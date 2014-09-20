475afd6f7381cca23bc25ec0dc214efa321826f4
Brett Lawson <brett19@gmail.com>
2014-09-20 19:26:34 -0300

    Cleaned up packet handling a bit.

--- client/js/net/gameclient.js
+++ client/js/net/gameclient.js
@@ -94,193 +94,196 @@ GameClient.prototype._emitPE = function(event, data) {
  * @private
  */
 GameClient.prototype._handlePacket = function(pak) {
-  var data = {};
-  switch (pak.cmd) {
-    case 0x715: {
-      // TODO: This packet is incorrect past headSize
-      data.gender = pak.readUint8();
-      data.zoneNo = pak.readInt32();
-      data.posStart = pak.readVector2().divideScalar(100);
-      data.reviveZoneNo = pak.readInt32();
-      data.parts = [];
-      for (var j = 0; j < AVTBODYPART.Max; ++j) {
-        data.parts.push(pak.readPartItem());
-      }
-      { // tagBasicINFO
-        data.birthStone = pak.readInt8();
-        data.faceIdx = pak.readInt8();
-        data.hairIdx = pak.readInt8();
-        data.job = pak.readInt16();
-        data.union = pak.readInt8();
-        data.rank = pak.readInt8();
-        data.fame = pak.readInt8();
-      }
-      { // tagBasicAbility
-        data.stats = {};
-        data.stats.str = pak.readInt16();
-        data.stats.dex = pak.readInt16();
-        data.stats.int = pak.readInt16();
-        data.stats.con = pak.readInt16();
-        data.stats.cha = pak.readInt16();
-        data.stats.sen = pak.readInt16();
-      }
-      { // tagGrowAbility
-        data.hp = pak.readInt32();
-        data.mp = pak.readInt32();
-        data.exp = pak.readUint32();
-        data.level = pak.readInt16();
-        data.bonusPoint = pak.readInt16();
-        data.skillPoint = pak.readInt16();
-        data.bodySize = pak.readUint8();
-        data.headSize = pak.readUint8();
-        data.penalExp = pak.readUint32();
-        data.fameG = pak.readInt16();
-        data.fameB = pak.readInt16();
-        data.pkFlag = pak.readInt16();
-        data.stamina = pak.readInt16();
-        data.patHp = pak.readInt32();
-        data.patCoolTime = pak.readUint32();
-      }
-      data.currency = [];
-      for (var r = 0; r < 10; ++r) {
-        data.currency.push(pak.readInt32());
-      }
-      data.maintainStatus = [];
-      for (var o = 0; o < 40; ++o) {
-        var mtns = {};
-        mtns.expireSec = pak.readUint32();
-        mtns.value = pak.readInt16();
-        mtns.dummy = pak.readInt16();
-        data.maintainStatus.push(mtns);
-      }
-      data.hotIcons = [];
-      for (var p = 0; p < 48; ++p) {
-        data.hotIcons.push(pak.readUint16());
-      }
-      data.uniqueTag = pak.readUint32();
-      data.coolTime = [];
-      for (var q = 0; q < 20; ++q) {
-        data.coolTime.push(pak.readUint32());
-      }
-      data.name = pak.readString();
-      this._emitPE('char_data', data);
-      break;
-    }
-    case 0x826: {
-      /*clientTime*/ pak.skip(8);
-      /*serverTime*/ pak.skip(8);
-      /*year*/ pak.readInt32();
-      /*month*/ pak.readInt32();
-      /*day*/ pak.readInt32();
-      /*hour*/ pak.readInt32();
-      /*min*/ pak.readInt32();
-      /*sec*/ pak.readInt32();
-      /*isDst*/ pak.readInt32(); // !== 0 (boolean)
-      this._emitPE('server_time', data);
-      break;
-    }
-    case 0x729: {
-      data.state = pak.readUint8();
-      this._emitPE('preload_char', data);
-      break;
-    }
-    case 0x724: {
-      data.result = pak.readUint8();
-      var wishCount = pak.readUint16();
-      data.items = [];
-      for (var i = 0; i < wishCount; ++i) {
-        data.items.push(pak.readItem());
-      }
-      this._emitPE('wish_list', data)
-      break;
-    }
-    case 0x716: {
-      data.result = pak.readUint8();
-      data.money = pak.readUint64();
-      var itemCount = pak.readUint16();
-      data.items = [];
-      for (var j = 0; j < itemCount; ++j) {
-        data.items.push(pak.readItem());
-      }
-      this._emitPE('inventory_data', data);
-      break;
-    }
-    case 0x855: {
-      data.result = pak.readUint8();
-      data.dailyQuests = pak.readUint32();
-      data.questCount = pak.readUint32();
-      this._emitPE('quest_completion_data', data);
-      break;
-    }
-    case 0x723: {
-      data.result = pak.readUint8();
-      var qitemCount = pak.readUint16();
-      data.items = [];
-      for (var k = 0; k < qitemCount; ++k) {
-        var questNo = pak.readInt32();
-        var qitem = pak.readItem();
-        qitem.questNo = questNo;
-        data.items.push(qitem);
-      }
-      this._emitPE('questitem_list', data);
-      break;
-    }
-    case 0x71b: {
-      // TODO: This packet...
-      data.result = pak.readUint8();
-      this._emitPE('quest_data', data);
-      break;
-    }
-    case 0x71a: {
-      data.result = pak.readUint8();
-      var skillCount = pak.readInt16();
-      data.skills = [];
-      for (var l = 0; l < skillCount; ++l) {
-        var skill = {};
-        skill.slot = pak.readInt16();
-        skill.skillIdx = pak.readInt16();
-        skill.expireSec = pak.readInt32();
-        data.skills.push(skill);
-      }
-      this._emitPE('skill_data', data);
-      break;
-    }
-    case 0x7ec: {
-      data.curHp = pak.readInt32();
-      data.curMp = pak.readInt32();
-      data.recoveryTickHp = pak.readInt32();
-      data.recoveryTickMp = pak.readInt32();
-      /*forceUpdate -- Not sure the size here*/
-      this._emitPE('char_hpmp_info', data);
-      break;
-    }
-    case 0x791: {
-      data.objectIdx = pak.readUint16();
-      data.position = pak.readVector2().divideScalar(100);
-      data.posTo = pak.readVector2().divideScalar(100);
-      data.command = pak.readUint16();
-      data.targetObj = pak.readUint16();
-      data.rideObj = pak.readUint16();
-      data.moveMode = pak.readUint8();
-      data.hp = pak.readInt32();
-      data.teamNo = pak.readInt32();
-      data.statusFlag = pak.readUint64();
-      data.statusTimers = [];
-      for (var si = 0; si < 66; ++si) {
-        data.statusTimers.push(pak.readInt16());
-      }
-      data.charIdx = pak.readInt16();
-      data.questIdx = pak.readInt16();
-      data.modelDir = pak.readFloat();
-      data.eventStatuses = [];
-      for (var ei = 0; ei < 5; ++ei) {
-        data.eventStatuses.push(pak.readInt16());
-      }
-      this._emitPE('spawn_npc_char', data);
-      break;
-    }
-
+  var packetHandler = GameClient.packetHandlers[pak.cmd];
+  if (packetHandler) {
+    packetHandler.call(this, pak, {});
   }
 };
+GameClient.packetHandlers = {};
+GameClient._registerHandler = function(cmd, handler) {
+  GameClient.packetHandlers[cmd] = handler;
+};
+
+GameClient._registerHandler(0x715, function(pak, data) {
+  data.gender = pak.readUint8();
+  data.zoneNo = pak.readInt32();
+  data.posStart = pak.readVector2().divideScalar(100);
+  data.reviveZoneNo = pak.readInt32();
+  data.parts = [];
+  for (var j = 0; j < AVTBODYPART.Max; ++j) {
+    data.parts.push(pak.readPartItem());
+  }
+  { // tagBasicINFO
+    data.birthStone = pak.readInt8();
+    data.faceIdx = pak.readInt8();
+    data.hairIdx = pak.readInt8();
+    data.job = pak.readInt16();
+    data.union = pak.readInt8();
+    data.rank = pak.readInt8();
+    data.fame = pak.readInt8();
+  }
+  { // tagBasicAbility
+    data.stats = {};
+    data.stats.str = pak.readInt16();
+    data.stats.dex = pak.readInt16();
+    data.stats.int = pak.readInt16();
+    data.stats.con = pak.readInt16();
+    data.stats.cha = pak.readInt16();
+    data.stats.sen = pak.readInt16();
+  }
+  { // tagGrowAbility
+    data.hp = pak.readInt32();
+    data.mp = pak.readInt32();
+    data.exp = pak.readUint32();
+    data.level = pak.readInt16();
+    data.bonusPoint = pak.readInt16();
+    data.skillPoint = pak.readInt16();
+    data.bodySize = pak.readUint8();
+    data.headSize = pak.readUint8();
+    data.penalExp = pak.readUint32();
+    data.fameG = pak.readInt16();
+    data.fameB = pak.readInt16();
+    data.pkFlag = pak.readInt16();
+    data.stamina = pak.readInt16();
+    data.patHp = pak.readInt32();
+    data.patCoolTime = pak.readUint32();
+  }
+  data.currency = [];
+  for (var r = 0; r < 10; ++r) {
+    data.currency.push(pak.readInt32());
+  }
+  data.maintainStatus = [];
+  for (var o = 0; o < 40; ++o) {
+    var mtns = {};
+    mtns.expireSec = pak.readUint32();
+    mtns.value = pak.readInt16();
+    mtns.dummy = pak.readInt16();
+    data.maintainStatus.push(mtns);
+  }
+  data.hotIcons = [];
+  for (var p = 0; p < 48; ++p) {
+    data.hotIcons.push(pak.readUint16());
+  }
+  data.uniqueTag = pak.readUint32();
+  data.coolTime = [];
+  for (var q = 0; q < 20; ++q) {
+    data.coolTime.push(pak.readUint32());
+  }
+  data.name = pak.readString();
+  this._emitPE('char_data', data);
+});
+
+GameClient._registerHandler(0x826, function(pak, data) {
+  /*clientTime*/ pak.skip(8);
+  /*serverTime*/ pak.skip(8);
+  /*year*/ pak.readInt32();
+  /*month*/ pak.readInt32();
+  /*day*/ pak.readInt32();
+  /*hour*/ pak.readInt32();
+  /*min*/ pak.readInt32();
+  /*sec*/ pak.readInt32();
+  /*isDst*/ pak.readInt32(); // !== 0 (boolean)
+  this._emitPE('server_time', data);
+});
+
+GameClient._registerHandler(0x729, function(pak, data) {
+  data.state = pak.readUint8();
+  this._emitPE('preload_char', data);
+});
+
+GameClient._registerHandler(0x724, function(pak, data) {
+  data.result = pak.readUint8();
+  var wishCount = pak.readUint16();
+  data.items = [];
+  for (var i = 0; i < wishCount; ++i) {
+    data.items.push(pak.readItem());
+  }
+  this._emitPE('wish_list', data)
+});
+
+GameClient._registerHandler(0x716, function(pak, data) {
+  data.result = pak.readUint8();
+  data.money = pak.readUint64();
+  var itemCount = pak.readUint16();
+  data.items = [];
+  for (var j = 0; j < itemCount; ++j) {
+    data.items.push(pak.readItem());
+  }
+  this._emitPE('inventory_data', data);
+});
+
+GameClient._registerHandler(0x855, function(pak, data) {
+  data.result = pak.readUint8();
+  data.dailyQuests = pak.readUint32();
+  data.questCount = pak.readUint32();
+  this._emitPE('quest_completion_data', data);
+});
+
+GameClient._registerHandler(0x723, function(pak, data) {
+  data.result = pak.readUint8();
+  var qitemCount = pak.readUint16();
+  data.items = [];
+  for (var k = 0; k < qitemCount; ++k) {
+    var questNo = pak.readInt32();
+    var qitem = pak.readItem();
+    qitem.questNo = questNo;
+    data.items.push(qitem);
+  }
+  this._emitPE('questitem_list', data);
+});
+
+GameClient._registerHandler(0x71b, function(pak, data) {
+  // TODO: This packet...
+  data.result = pak.readUint8();
+  this._emitPE('quest_data', data);
+});
+
+GameClient._registerHandler(0x71a, function(pak, data) {
+  data.result = pak.readUint8();
+  var skillCount = pak.readInt16();
+  data.skills = [];
+  for (var l = 0; l < skillCount; ++l) {
+    var skill = {};
+    skill.slot = pak.readInt16();
+    skill.skillIdx = pak.readInt16();
+    skill.expireSec = pak.readInt32();
+    data.skills.push(skill);
+  }
+  this._emitPE('skill_data', data);
+});
+
+GameClient._registerHandler(0x7ec, function(pak, data) {
+  data.curHp = pak.readInt32();
+  data.curMp = pak.readInt32();
+  data.recoveryTickHp = pak.readInt32();
+  data.recoveryTickMp = pak.readInt32();
+  /*forceUpdate -- Not sure the size here*/
+  this._emitPE('char_hpmp_info', data);
+});
+
+GameClient._registerHandler(0x791, function(pak, data) {
+  data.objectIdx = pak.readUint16();
+  data.position = pak.readVector2().divideScalar(100);
+  data.posTo = pak.readVector2().divideScalar(100);
+  data.command = pak.readUint16();
+  data.targetObj = pak.readUint16();
+  data.rideObj = pak.readUint16();
+  data.moveMode = pak.readUint8();
+  data.hp = pak.readInt32();
+  data.teamNo = pak.readInt32();
+  data.statusFlag = pak.readUint64();
+  data.statusTimers = [];
+  for (var si = 0; si < 66; ++si) {
+    data.statusTimers.push(pak.readInt16());
+  }
+  data.charIdx = pak.readInt16();
+  data.questIdx = pak.readInt16();
+  data.modelDir = pak.readFloat();
+  data.eventStatuses = [];
+  for (var ei = 0; ei < 5; ++ei) {
+    data.eventStatuses.push(pak.readInt16());
+  }
+  this._emitPE('spawn_npc_char', data);
+});
 
 var netGame = null;
