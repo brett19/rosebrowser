91c1c2736ce2516a4f6d9e28490b6ac455cb6921
Jean Guegant <jguegant@gmail.com>
2014-09-22 21:23:57 +0200

    Fix the rotation issues on a moveable object.

--- client/js/objects/moveableobject.js
+++ client/js/objects/moveableobject.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// http://javascript.about.com/od/problemsolving/a/modulobug.htm
+function mod(x, n) {
+  return ((x % n) + n) %n;
+}
+
 function MoveableObject(type, world) {
   GameObject.call(this, type, world);
 
@@ -19,7 +24,7 @@ MoveableObject.prototype.moveTo = function(x, y) {
 };
 
 MoveableObject.prototype.setDirection = function(radians) {
-  this.direction = radians;
+  this.direction = mod(radians, (Math.PI * 2));
   this.emit('moved');
 };
 
@@ -43,27 +48,68 @@ MoveableObject.prototype.update = function(delta) {
       if (!this.useMoveCollision || true) {
         newPosition.z = highZ;
 
-        var posDiff = new THREE.Vector2(
-            this.position.x-newPosition.x,
-            this.position.y-newPosition.y);
-        var newDirection = Math.atan2(posDiff.y, posDiff.x) - Math.PI/2;
+        var newDirection = Math.atan2(this.velocity.y, this.velocity.x) + Math.PI/2;
+
+        newDirection = mod(newDirection, (Math.PI * 2));
 
         var dirDelta = newDirection - this.direction;
-        if (dirDelta < -Math.PI) {
-          dirDelta += Math.PI * 2;
-        } else if(dirDelta > Math.PI) {
-          dirDelta -= Math.PI * 2;
+
+        var dirStep = (Math.PI * 3) * delta;
+
+        // Note: In order to avoid the spins problems, and ease the computations,
+        // the direction is bounded between 0 and 2 * PI.
+
+        if (dirDelta > 0) {
+          // If the newDirection get an angle greater than before.
+          if (dirDelta > Math.PI) {
+            // If the difference of direction is greater than PI.
+            // The shortest path is to rotate backward until we go below 0.
+            if (this.direction - dirStep >= 0) {
+              this.direction -= dirStep;
+            } else {
+              // If the step goes below 0, we must ensure we didn't rotate too much,
+              // as well as applying the direction bounds.
+              var postiveDirect = mod((this.direction - dirStep), (Math.PI * 2));
+              if (postiveDirect > newDirection) {
+                this.direction = postiveDirect;
+              } else {
+                this.direction = newDirection;
+              }
+            }
+          } else {
+            // Otherwise we wimply rotate forward.
+            if (this.direction + dirStep > newDirection) {
+              this.direction = newDirection;
+            } else {
+              this.direction += dirStep;
+            }
+          }
+        } else {
+          // If the newDirection get an angle smaller than before.
+          if (dirDelta < - Math.PI) {
+            // If the difference of direction is greater than PI.
+            // The shortest path is to rotate forward until we go further than 2 * PI.
+            if (this.direction + dirStep < Math.PI * 2) {
+              this.direction += dirStep;
+            } else {
+              // If the step goes over 2 * PI, we must ensure we didn't rotate too much,
+              // as well as applying the direction bounds.
+              var lessthan2piDirect = mod((this.direction + dirStep), (Math.PI * 2));
+              if (lessthan2piDirect < newDirection) {
+                this.direction = lessthan2piDirect;
+              } else {
+                this.direction = newDirection;
+              }
+            }
+          } else {
+            // Otherwise we wimply rotate backward.
+            if (this.direction - dirStep < newDirection) {
+              this.direction = newDirection;
+            } else {
+              this.direction -= dirStep;
+            }            
+          }
         }
-
-        var maxDelta = (Math.PI * 3) * delta;
-        if (dirDelta < -maxDelta) {
-          dirDelta = -maxDelta;
-        } else if (dirDelta > maxDelta) {
-          dirDelta = maxDelta;
-        }
-
-        this.direction += dirDelta;
-
 
         this.position.copy(newPosition);
         this.emit('moved');
