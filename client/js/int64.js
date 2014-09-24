6d0a878e42ea0e3a2115767d5f8fed568172fdb3
Brett Lawson <brett19@gmail.com>
2014-09-24 00:29:33 -0300

    Added more Int64 handling methods.

--- client/js/int64.js
+++ client/js/int64.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// TODO: Make everything handle signed int64 properly...
+
 function Int64(lo, hi) {
   this.lo = lo;
   this.hi = hi;
@@ -11,4 +13,22 @@ Int64.prototype.isBitSet = function(bit) {
   } else {
     return this.hi & (1 << (bit-32));
   }
+};
+
+Int64.prototype.toString = function(radix) {
+  if (radix === 10 || radix === undefined) {
+    return '?int64_tostring_badradix?';
+  } else if (radix === 16) {
+    var str = this.lo.toString(16);
+    while (str.length < 8) {
+      str = '0' + str;
+    }
+    return this.hi.toString(16) + str;
+  } else {
+    return '?int64_tostring_badradix?';
+  }
+};
+
+Int64.prototype.toNumber = function() {
+  return this.hi << 32 | this.lo;
 };
