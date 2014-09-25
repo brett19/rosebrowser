07b845fc27fdd3ef75f517e6554a1fef762233b7
Brett Lawson <brett19@gmail.com>
2014-09-25 00:37:07 -0300

    Make RoseTexManager use global normalizePath.

--- client/js/helpers/rosetexmanager.js
+++ client/js/helpers/rosetexmanager.js
@@ -4,10 +4,6 @@ function _RoseTextureManager() {
   this.textures = {};
 }
 
-_RoseTextureManager.prototype.normalizePath = function(path) {
-  return path;
-};
-
 _RoseTextureManager.prototype._load = function(path, callback) {
   var tex = DDS.load(path, function() {
     if (callback) {
@@ -19,7 +15,7 @@ _RoseTextureManager.prototype._load = function(path, callback) {
 };
 
 _RoseTextureManager.prototype.load = function(path, callback) {
-  var normPath = this.normalizePath(path);
+  var normPath = normalizePath(path);
 
   var foundTex = this.textures[normPath];
   if (foundTex) {
