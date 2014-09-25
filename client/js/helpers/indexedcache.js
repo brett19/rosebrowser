21a1d781271920ae1bc65d53523613516af8aab0
Brett Lawson <brett19@gmail.com>
2014-09-25 03:34:22 -0300

    Fix various bugs in the IndexedCache.

--- client/js/helpers/indexedcache.js
+++ client/js/helpers/indexedcache.js
@@ -1,28 +1,39 @@
 'use strict';
 
 function IndexedCache(getFunc) {
-  this.items = {};
-  this.getFunc = getFunc;
+  this._items = {};
+  this._getFunc = getFunc;
 }
 
+IndexedCache.prototype.getNow = function(index) {
+  var item = this._items[index];
+  if (!item || !item.data) {
+    throw new Error('Attempted to getNow an unloaded data resource (' + index + ').');
+  }
+
+  return item.data;
+};
+
 IndexedCache.prototype.get = function(index, callback) {
-  var item = this.items[index];
+  var item = this._items[index];
   if (item) {
-    if (item.data) {
-      callback(item.data);
-    } else {
-      item.waiters.push(callback);
+    if (callback) {
+      if (item.data) {
+        callback(item.data);
+      } else {
+        item.waiters.push(callback);
+      }
     }
     return;
   }
 
   var newItem = {
     data: null,
-    waiters: [callback]
+    waiters: callback ? [callback] : []
   };
-  this.items[index] = newItem;
+  this._items[index] = newItem;
 
-  this.getFunc(index, function(data) {
+  this._getFunc(index, function(data) {
     newItem.data = data;
     for (var i = 0; i < newItem.waiters.length; ++i) {
       newItem.waiters[i](data);
@@ -34,9 +45,9 @@ IndexedCache.prototype.get = function(index, callback) {
 
 // Little helper for dealing with path based indexes.
 function DataCache(loader) {
-  this.cache = new IndexedCache(loader.load);
+  this._cache = new IndexedCache(loader.load);
 }
 DataCache.prototype.get = function(path, callback) {
   var normPath = normalizePath(path);
-  return this.cache.get(normPath, callback);
+  return this._cache.get(normPath, callback);
 };
