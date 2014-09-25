7759dad9d1efdbbc722e6fac2361d610e5a6dd29
Brett Lawson <brett19@gmail.com>
2014-09-25 04:34:36 -0300

    Added a global debug helper to print camera location.

--- client/js/helpers/debughelper.js
+++ client/js/helpers/debughelper.js
@@ -4,10 +4,16 @@ function _DebugHelper() {
 }
 
 _DebugHelper.prototype.init = function() {
-  debugGui.add(this, 'printScene');
+  debugGui.add(this, 'logCameraLocation');
+  debugGui.add(this, 'logScene');
 };
 
-_DebugHelper.prototype.printScene = function() {
+_DebugHelper.prototype.logCameraLocation = function() {
+  var worldPos = camera.localToWorld(new THREE.Vector3(0, 0, 0));
+  console.log('Camera Location:', worldPos.x, worldPos.y, worldPos.z);
+};
+
+_DebugHelper.prototype.logScene = function() {
   function _printThis(obj) {
     var out = '';
     out += 'OBJ';
