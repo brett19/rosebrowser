3de02bb78a74dbb1f33ba38c11c3d2b898a22b9f
Brett Lawson <brett19@gmail.com>
2014-09-28 04:44:37 -0300

    Added support for setMotion on NpcPawn's.

--- client/js/pawns/npcpawn.js
+++ client/js/pawns/npcpawn.js
@@ -1,17 +1,112 @@
 'use strict';
 
+var NPCANI = {
+  STOP: 0,
+  MOVE: 1,
+  ATTACK: 2,
+  HIT: 3,
+  DIE: 4,
+  RUN: 5,
+  CACTION1: 6,
+  SACTION1: 7,
+  CACTION2: 8,
+  SACTION2: 9,
+  ETC:10,
+  MAX: 11
+};
+
 function NpcPawn(go) {
   this.rootObj = new THREE.Object3D();
   this.rootObj.owner = this;
+  this.skel = null;
+  this.charIdx = 0;
+  this.motionCache = new IndexedCache(this._loadMotion.bind(this));
+  this.activeMotionIdx = -1;
+  this.activeMotion = null;
+  this.prevMotion = null;
 
   if (go) {
     this.owner = go;
 
+    var self = this;
     this.rootObj.name = 'NPC_' + go.serverObjectIdx + '_' + go.charIdx;
     this.rootObj.rotation.z = go.direction;
     this.setModel(go.charIdx);
+
+    go.on('start_move', function() {
+      self.setMotion(NPCANI.MOVE);
+    });
+    go.on('stop_move', function() {
+      self.setMotion(NPCANI.STOP);
+    });
+    go.on('moved', function () {
+      self.rootObj.position.copy(go.position);
+      self.rootObj.rotation.z = go.direction;
+    });
   }
 }
+
+/**
+ * Holds a cache of all loaded animation files data.  This is just the data,
+ * and not bound to any particular skeleton.
+ *
+ * @type {DataCache.<AnimationData>}
+ */
+NpcPawn.motionFileCache = new DataCache(AnimationData);
+
+// This function should never be called directly, and should only used
+//   by the loadedMotions cache.  Use this.motionCache.get instead.
+NpcPawn.prototype._loadMotion = function(actionIdx, callback) {
+  var self = this;
+  GDM.get('npc_chars', function(charData) {
+    var char = charData.characters[self.charIdx];
+    var animIdx = char.animations[actionIdx];
+    var motionFile = charData.animations[animIdx];
+
+    NpcPawn.motionFileCache.get(motionFile, function(animData) {
+      var anim = new SkeletonAnimator(self.skel, animData);
+      callback(anim);
+    });
+  });
+};
+
+NpcPawn.prototype.setMotion = function(actionIdx, callback) {
+  this.activeMotionIdx = actionIdx;
+
+  var self = this;
+  this.motionCache.get(actionIdx, function(anim) {
+    // Don't overwrite any newer motion changes.
+    if (actionIdx !== self.activeMotionIdx) {
+      return;
+    }
+
+    if (self.activeMotion === anim) {
+      // Already playing this animation!
+      return;
+    }
+
+    self.prevMotion = self.activeMotion;
+
+    self.activeMotion = anim;
+
+    // TODO: Accessing owner like this is unsafe for non-GO based pawns.
+    if (self.owner) {
+      var moveAnimScale = (self.owner.moveSpeed + 180) / 600;
+      anim.timeScale = moveAnimScale;
+    }
+
+    anim.play();
+    if (self.prevMotion) {
+      self.activeMotion.weight = 1 - self.prevMotion.weight;
+    } else {
+      self.activeMotion.weight = 1;
+    }
+
+    if (callback) {
+      callback();
+    }
+  });
+};
 
 NpcPawn.prototype._setModel = function(charData, modelMgr, charIdx) {
   var self = this;
@@ -26,6 +121,7 @@ NpcPawn.prototype._setModel = function(charData, modelMgr, charIdx) {
 
   SkeletonData.load(skelPath, function(zmdData) {
     var charSkel = zmdData.create(self.rootObj);
+    self.skel = charSkel;
 
     var charModels = char.models;
     for (var i = 0; i < charModels.length; ++i) {
@@ -48,11 +144,7 @@ NpcPawn.prototype._setModel = function(charData, modelMgr, charIdx) {
       }
     }
 
-    var animPath = charData.animations[char.animations[0]];
-    AnimationData.load(animPath, function(zmoData) {
-      var anim = new SkeletonAnimator(charSkel, zmoData);
-      anim.play();
-    });
+    self.setMotion(NPCANI.STOP);
 
     for (var e = 0; e < char.effects.length; ++e) {
       var effectPath = charData.effects[char.effects[e].effectIdx];
@@ -66,6 +158,7 @@ NpcPawn.prototype._setModel = function(charData, modelMgr, charIdx) {
 
 NpcPawn.prototype.setModel = function(charIdx, callback) {
   var self = this;
+  this.charIdx = charIdx;
   GDM.get('npc_chars', 'npc_models', function(charList, modelList) {
     self._setModel(charList, modelList, charIdx);
     if (callback) {
@@ -75,5 +168,16 @@ NpcPawn.prototype.setModel = function(charIdx, callback) {
 };
 
 NpcPawn.prototype.update = function(delta) {
+  var blendWeightDelta = 6 * delta;
 
+  if (this.prevMotion && this.activeMotion) {
+    this.prevMotion.weight -= blendWeightDelta;
+    this.activeMotion.weight += blendWeightDelta;
+
+    if (this.activeMotion.weight >= 1.0) {
+      this.activeMotion.weight = 1.0;
+      this.prevMotion.stop();
+      this.prevMotion = null;
+    }
+  }
 };
