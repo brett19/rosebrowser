c7458abe5e849b3b902f4365b83e8d36ec1e686c
James Benton <james.benton2@gmail.com>
2014-10-04 16:07:25 +0100

    Update character select dialog to have create delete and cancel buttons.

--- client/gui/dialogs/characterselect.js
+++ client/gui/dialogs/characterselect.js
@@ -6,6 +6,11 @@ ui.CharacterSelectDialog = function(template, characters) {
   this._characterList = ui.list(this, '.list.characters');
   this._characterList.on('itemclicked', this._changeCharacter.bind(this));
   this._characterList.on('itemdoubleclicked', this._selectCharacter.bind(this));
+
+  ui.button(this, '.button.create').on('clicked', this._onCreateClicked.bind(this));
+  ui.button(this, '.button.delete').on('clicked', this._onDeleteClicked.bind(this));
+  ui.button(this, '.button.back').on('clicked', this._onBackClicked.bind(this));
+
   this._characters = characters;
   this._update();
 };
@@ -36,6 +41,18 @@ ui.CharacterSelectDialog.prototype._selectCharacter = function(index) {
   index = index || this._characterList.index();
   this.emit('done', this._characters[index].name);
   this.close();
+};
+
+ui.CharacterSelectDialog.prototype._onCreateClicked = function() {
+  this.emit('create');
+};
+
+ui.CharacterSelectDialog.prototype._onDeleteClicked = function() {
+  this.emit('delete');
+};
+
+ui.CharacterSelectDialog.prototype._onBackClicked = function() {
+  this.emit('cancel');
 };
 
 ui.characterSelectDialog = function(characters) {
