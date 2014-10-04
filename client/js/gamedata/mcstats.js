63a29b6ac51a10603479a58f0c8ad0c041294700
Brett Lawson <brett19@gmail.com>
2014-10-04 02:50:36 -0300

    Added Needed EXP calculation.
    Note!  naROSE has changed how a lot of these calculations work now :(

--- client/js/gamedata/mcstats.js
+++ client/js/gamedata/mcstats.js
@@ -39,8 +39,13 @@ McStats.prototype._getEquipData = function(equipIdx) {
   return itemData.getData(item.itemType, item.itemNo);
 };
 
-McStats.prototype.getMaxHp = function() { return 12337; };
-McStats.prototype.getMaxMp = function() { return 12337; };
+McStats.prototype.getMaxHp = function() {
+  return 12338;
+};
+
+McStats.prototype.getMaxMp = function() {
+  return 12338;
+};
 
 McStats.prototype.getAttackSpeed = function() {
   // TODO: Implement this properly!
@@ -53,4 +58,19 @@ McStats.prototype.getAttackDistance = function() {
     return DEFAULT_ATTACK_DISTANCE;
   }
   return weaponData[WEAPON_DATA.RANGE] / 100;
+};
+
+McStats.getNeedRawExp = function () {
+  var level = this.object.level;
+  if (level <= 15) {
+    return Math.floor((level + 3) * (level + 5) * (level + 10) * 0.7);
+  } else if (level <= 50) {
+    return Math.floor((level - 5) * (level + 2) * (level + 2) * 2.2);
+  } else if (level <= 100) {
+    return Math.floor((level - 5) * (level + 2) * (level - 38) * 9.0);
+  } else if (level <= 139) {
+    return Math.floor((level + 27) * (level + 34) * (level + 220) * 1.0);
+  } else {
+    return Math.floor((level - 15) * (level + 7) * (level - 126) * 41.0);
+  }
 };
