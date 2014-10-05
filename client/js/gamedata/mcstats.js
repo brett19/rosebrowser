b4617e442c0aec9763d79fe6efdd0baef37964fa
Brett Lawson <brett19@gmail.com>
2014-10-05 17:27:45 -0300

    Added MaxHP and MaxMP calculation.

--- client/js/gamedata/mcstats.js
+++ client/js/gamedata/mcstats.js
@@ -40,11 +40,63 @@ McStats.prototype._getEquipData = function(equipIdx) {
 };
 
 McStats.prototype.getMaxHp = function() {
-  return 12338;
+  var iA = 10, iM1 = 8, fC = 2.25;
+  var job = this.object.job;
+  var level = this.object.level;
+
+  switch(job) {
+    case 111: iA=15; fC=3.50; iM1=12; break;
+    case 121: iA=20; fC=5.00; iM1=20; break;
+    case 122: iA=15; fC=4.00; iM1=18; break;
+
+    case 211: iA=8; fC=3.25; iM1=10; break;
+    case 221: iA=8; fC=3.50; iM1=15; break;
+    case 222: iA=8; fC=3.75; iM1=17; break;
+
+    case 311: iA=10; fC=3.00; iM1=10; break;
+    case 321: iA=15; fC=3.25; iM1=18; break;
+    case 322: iA=12; fC=3.00; iM1=16; break;
+
+    case 411: iA=12; fC=3.00; iM1=10; break;
+    case 421: iA=13; fC=4.00; iM1=19; break;
+    case 422: iA=15; fC=3.75; iM1=16; break;
+
+    case 411: iA=12; fC=3.00; iM1=10; break;
+    case 421: iA=13; fC=4.00; iM1=19; break;
+    case 422: iA=15; fC=3.75; iM1=17; break;
+
+    default: iA=10; fC=2.25; iM1=8; break;
+  }
+
+  return Math.floor((level + iA) * Math.sqrt(level + iM1) * fC + (this.str * fC));
 };
 
 McStats.prototype.getMaxMp = function() {
-  return 12338;
+  var iA = 5, fM1 = 3.0, fM2 = 2.0;
+  var job = this.object.job;
+  var level = this.object.level;
+
+  switch (job) {
+    case 111: iA=10; fM1=3.0; fM2=2.50; break;
+    case 121: iA=10; fM1=3.5; fM2=3.25; break;
+    case 122: iA=10; fM1=4.0; fM2=2.75; break;
+
+    case 211: iA=20; fM1=8.0; fM2=4.00; break;
+    case 221: iA=20; fM1=10.0; fM2=5.00; break;
+    case 222: iA=20; fM1=9.0; fM2=5.00; break;
+
+    case 311: iA=15; fM1=4.0; fM2=2.50; break;
+    case 321: iA=15; fM1=5.0; fM2=2.75; break;
+    case 322: iA=15; fM1=6.0; fM2=3.25; break;
+
+    case 411: iA=12; fM1=5.0; fM2=2.75; break;
+    case 421: iA=12; fM1=6.0; fM2=3.25; break;
+    case 422: iA=12; fM1=6.0; fM2=3.75; break;
+
+    default: iA=5; fM1=3.0; fM2=2.0; break;
+  }
+
+  return Math.floor((level + iA) * fM1 + (this.int * fM2));
 };
 
 McStats.prototype.getAttackSpeed = function() {
