3480bf8a6de2c0dd46b7b5e0a1e2a968ae4e603c
James Benton <james.benton2@gmail.com>
2014-10-07 15:33:41 +0100

    Make addEventListener return this for function chaining.

--- client/js/util/eventemitter.js
+++ client/js/util/eventemitter.js
@@ -11,10 +11,12 @@ EventEmitter.prototype.emit = function(event) {
   if (!this.eventHandlers[event]) {
     return;
   }
+
   var argsOut = [];
   for (var i = 1; i < arguments.length; ++i) {
     argsOut.push(arguments[i]);
   }
+
   for (var j = 0; j < this.eventHandlers[event].length; ++j) {
     this.eventHandlers[event][j].apply(this, argsOut);
   }
@@ -24,19 +26,24 @@ EventEmitter.prototype.addEventListener = function(event, handler) {
   if (!this.eventHandlers[event]) {
     this.eventHandlers[event] = [];
   }
+
   this.eventHandlers[event].push(handler);
+  return this;
 };
+
 EventEmitter.prototype.on = EventEmitter.prototype.addEventListener;
 
 EventEmitter.prototype.removeEventListener = function(event, handler) {
   if (!this.eventHandlers[event]) {
     return false;
   }
+
   var handlerIdx = this.eventHandlers[event].indexOf(handler);
   if (handlerIdx !== -1) {
     this.eventHandlers[event].splice(handlerIdx, 1);
     return true;
   }
+
   return false;
 };
 
@@ -45,5 +52,7 @@ EventEmitter.prototype.once = function(event, handler) {
     this.removeEventListener(event, onceHandler);
     handler.apply(this, arguments);
   }.bind(this);
+
   this.addEventListener(event, onceHandler);
+  return this;
 };
